f0832bf30c9e3a7a971defaf6815d7a2069f17d8
Akash Kumar Gond <aakashgond759@gmail.com>
2024-05-16 01:42:53 +0530

    board created

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -5,3 +5,23 @@
  it wil also keep a move log.
     
 """
+class GameState():
+    def __init__(self):
+        # board is 8x8 2d list, each element of the list has 2 characters.
+        # The first character represents the color of the piece, 'b' or 'w'
+        # The second character represents the type of the piece, 'K', 'Q', 'R', 'B', 'N' or 'P'
+        # "--" represents an empty space with no piece.
+        
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+
+        self.WhiteToMove = True
+        self.moveLog = []
--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -3,3 +3,81 @@ this is our main driver file.
 it will be responsible for handoling user input and displaying thr current GameState object.
 
 """
+import pygame as p
+import ChessEngine
+
+
+WIDTH = HEIGHT = 512 # 400 is another option
+DIMENSIONS = 8 # dimensions of a chess board are 8x8
+SQ_SIZE = HEIGHT // DIMENSIONS
+MAX_FPS = 15 # for animation later on
+IMAGES = {}
+
+'''
+initialize a global dictionary of images. this will be called excatly in the main
+'''
+
+def  loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        IMAGES[piece] = p.transform.scale(p.image.load("CHess/images/" + piece + ".png"), (SQ_SIZE,  SQ_SIZE))
+        
+    #IMAGES['bp'] = p.image.load("images/bp.png")
+    # Note : we can access n image by saying 'IMAGES['wp']
+    
+    
+'''
+The main driver for our code. this will handle user input and udpating the graphics
+'''
+
+def main():
+    p.init()
+    screen = p.display.set_mode((WIDTH, HEIGHT))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState()
+    
+    #print(gs.board)
+    
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        
+        drawGameState(screen, gs)
+        clock.tick(MAX_FPS)
+        p.display.flip()
+    
+'''
+Responsible for all graphics within a current game state.
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen)  # draw squares on the board
+    #add in piece highlighting or move suggestions     
+    drawPieces(screen, gs.board) # draw the pieces on top of those squares
+
+'''
+Draw the squares on the board, the top left square is always light
+'''
+
+def drawBoard(Screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(DIMENSIONS):
+        for c in range(DIMENSIONS):
+            color = colors[ (r+c) % 2  ]
+            p.draw.rect(Screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            
+    
+'''
+Draw the pieces on the board using the current GameState.board
+'''
+    
+def drawPieces(screen, board):pass
+
+
+
+
+if __name__ == "__main__":
+    main()
