714b6db429fe3f4a10de56b8b5ed0a9797dfd82c
Akash Kumar Gond <aakashgond759@gmail.com>
2024-05-16 00:33:56 +0530

    Initail Commit

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,7 @@
+"""
+    
+ this class is responsible for storing all the information about the state of a chess game.
+ it will also be responsible for determining the valid at the current state.
+ it wil also keep a move log.
+    
+"""
---
+++ Chess/ChessMain.py
@@ -0,0 +1,5 @@
+"""
+this is our main driver file.
+it will be responsible for handoling user input and displaying thr current GameState object.
+
+"""
